12dac999666830da32c89ab20b5f2269a0eb4760
Kumar Kalyan <67071462+kum9748ar@users.noreply.github.com>
2021-08-10 20:35:18 +0530

    Add files via upload

---
+++ boilerplate-project-filemetadata/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+var multer = require('multer');
+var upload = multer({ dest: './uploads' })
+var app = express();
+var fs = require("fs");
+var path = require("path");
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  })
+
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
